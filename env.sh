b076c3d8110b591e9cf4219e61adf5ce3af2d13f
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-07-25 12:01:51 +0600

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# virtualenv env
+. env/bin/activate
+
+# reg your app at 
+export consumer_key=
+export consumer_secret=
+
+# pip install -r requirements.txt
+# python -m twitterbot_utils.TwiAuth
+export user_access_secret=
+export user_access_token=
